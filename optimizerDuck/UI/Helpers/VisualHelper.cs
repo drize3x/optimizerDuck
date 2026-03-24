985b694c9e542dc1202330a9666213899c5cffe5
这是drzy <drizelocked@gmail.com>
2026-03-24 16:11:25 +0700

    optimize animation and startup animation
    if you using win 11 or higher, animation working so fine

---
+++ optimizerDuck/UI/Helpers/VisualHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace optimizerDuck.UI.Helpers;
+
+public static class VisualHelper
+{
+    public static Frame? FindMainFrame(DependencyObject parent)
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+
+            if (child is Frame frame && frame.Name == "PART_Frame")
+                return frame;
+
+            var result = FindMainFrame(child);
+            if (result != null)
+                return result;
+        }
+
+        return null;
+    }
+}
--- optimizerDuck/UI/Windows/MainWindow.xaml.cs
+++ optimizerDuck/UI/Windows/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
-using optimizerDuck.Domain.Abstractions;
+﻿using optimizerDuck.Domain.Abstractions;
 using optimizerDuck.Services;
+using optimizerDuck.UI.Helpers;
 using optimizerDuck.UI.Pages;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using Wpf.Ui;
 using Wpf.Ui.Abstractions;
 
@@ -9,8 +13,14 @@ namespace optimizerDuck.UI.Windows;
 
 public partial class MainWindow : IWindow
 {
-    public MainWindow(INavigationService navigationService, IContentDialogService contentDialogService,
-        INavigationViewPageProvider pageProvider, ISnackbarService snackbarService, FeatureRegistry featureRegistry)
+    private Frame? _frame;
+
+    public MainWindow(
+        INavigationService navigationService,
+        IContentDialogService contentDialogService,
+        INavigationViewPageProvider pageProvider,
+        ISnackbarService snackbarService,
+        FeatureRegistry featureRegistry)
     {
         InitializeComponent();
 
@@ -21,14 +31,45 @@ public partial class MainWindow : IWindow
         RootNavigation.SetPageProviderService(pageProvider);
 
         var toggleItems = featureRegistry.GetNavigationItems();
-        foreach (var item in toggleItems) FeaturesMenuItem.MenuItems.Add(item);
+        foreach (var item in toggleItems)
+            FeaturesMenuItem.MenuItems.Add(item);
 
         RootNavigation.Loaded += OnRootNavigationLoaded;
+
     }
 
     private void OnRootNavigationLoaded(object sender, RoutedEventArgs e)
     {
         RootNavigation.Loaded -= OnRootNavigationLoaded;
+
+        _frame = VisualHelper.FindMainFrame(RootNavigation);
+
+        if (_frame != null)
+            _frame.Navigated += Frame_Navigated;
+
         RootNavigation.Navigate(typeof(DashboardPage));
+    }
+
+    private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+    {
+        if (e.Content is not Page page)
+            return;
+
+        var transform = new ScaleTransform(0.96, 0.96);
+        page.RenderTransform = transform;
+        page.RenderTransformOrigin = new Point(0.5, 0.5);
+
+        page.Opacity = 0;
+
+        var fade = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(200));
+
+        var scale = new DoubleAnimation(0.96, 1, TimeSpan.FromMilliseconds(200))
+        {
+            EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+        };
+
+        page.BeginAnimation(UIElement.OpacityProperty, fade);
+        transform.BeginAnimation(ScaleTransform.ScaleXProperty, scale);
+        transform.BeginAnimation(ScaleTransform.ScaleYProperty, scale);
     }
 }
